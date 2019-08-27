7dee56bf7dafa5d687c30c526519de4c4f457279
Andydenike <denikeandrew@gmail.com>
2019-08-27 16:01:55 -0700

    Added functionalities

--- app.js
+++ app.js
@@ -1,20 +1,66 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const paper_div = document.getElementById("p")
 const scissor_div = document.getElementById("s")
 const rock_div = document.getElementById("r")
 
 
 function getComputerChoice() {
-    
+    const choices = ['r', 'p', 's']
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convert2Word(letter) {
+    if (letter === "r") return "Rock"
+    if (letter === "p") return "Paper"
+    return "Scissors"
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore 
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convert2Word(userChoice)}${smallUserWord} beats ${convert2Word(computerChoice)}${smallCompWord}. You win the chocolate factory!`;
+    // result_p.innerHTML = convert2Word(userChoice) + " beats " + convert2Word(computerChoice) + ". You win the chocolate factory!";
+}
+
+function lose() {
+    userScore++;
+    userScore_span.innerHTML = userScore 
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convert2Word(userChoice)} beats ${convert2Word(computerChoice)}. You win the chocolate factory!`;
+    // result_p.innerHTML = convert2Word(userChoice) + " beats " + convert2Word(computerChoice) + ". You win the chocolate factory!";
+}
+
+function draw() {
+    console.log("DRAW")
 }
 
 function game(userChoice) {
-    console.log("poop " + userChoice);
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "sp":
+        case "pr":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "pp":
+        case "ss":
+        case "rr":
+            draw(userChoice, computerChoice);
+            break;
+    }
 }
 
 function main() {
